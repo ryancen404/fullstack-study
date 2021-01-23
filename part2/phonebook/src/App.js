8b5e1872a30e1d1ba9f7f6ad436aa49882e8cf7a
ryan <ryancen404@gmail.com>
2021-01-23 18:45:09 +0800

    part2 2.10

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,5 +1,33 @@
 import React, { useState } from 'react'
 
+const Filter = ({text, onChange}) => {
+    return(
+        <div>
+          {"filter shown with "} <input value = {text} onChange = {onChange} />
+        </div>
+    )
+}
+
+const PersonForm = ({nameText, numberText, onNameChang, onNumberChange, onSubmit}) => {
+    return (
+        <form onSubmit = {onSubmit}>
+        <div>
+            {"name: "} <input value = {nameText} onChange = {onNameChang}/>
+        </div>
+        <div>
+            {"number: "} <input value = {numberText} onChange = {onNumberChange}/>
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+    )
+}
+
+const Persons = ({ persons }) => {
+    return (persons.map(p =><div key = {p.name}> {p.name} {p.number} </div>))
+}
+
 const App = () => {
   const [persons, setPersons] = useState([
     { name: 'Arto Hellas', number: '040-123456' },
@@ -37,12 +65,11 @@ const App = () => {
       }
   }
 
-  const getShowNumbers = () => {
+  const filterPersons = () => {
       if (search === '') {
-          return persons.map(p =><div key = {p.name}> {p.name} {p.number} </div>)
+          return persons
       }
       const lowerCase = search.toLowerCase()
-      console.log(lowerCase);
       let match = []
       persons.forEach (p => {
         let name = p.name.toLowerCase()
@@ -55,30 +82,27 @@ const App = () => {
             match.push(p)
         }
       })
-      console.log(match);
-      return match.map(p =><div key = {p.name}> {p.name} {p.number} </div>)
+      return match
   }
 
   return (
     <div>
       <h2>Phonebook</h2>
-      <div>
-          {"filter shown with "} <input value = {search} onChange = {handleSearchChange} />
-      </div>
-      <h2>{"add a new"}</h2>
-      <form onSubmit = {addPerson}>
-        <div>
-            {"name: "} <input value = {newName} onChange = {handleNameChange}/>
-        </div>
-        <div>
-            {"number: "} <input value = {newNumber} onChange = {handleNumberChange}/>
-        </div>
-        <div>
-          <button type="submit">add</button>
-        </div>
-      </form>
-      <h2>Numbers</h2>
-      {getShowNumbers()}
+
+      <Filter text = {search} onChange = {handleSearchChange} />
+
+      <h3>{"add a new"}</h3>
+
+      <PersonForm
+        nameText = {newName}
+        numberText = {newNumber}
+        onNameChang = {handleNameChange}
+        onNumberChange = {handleNumberChange}
+        onSubmit = {addPerson}
+        />
+      <h3>Numbers</h3>
+      
+      <Persons persons = {filterPersons()} />
     </div>
   )
 }
